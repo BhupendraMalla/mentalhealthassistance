48fee463fd040808dec6d014ac0bf575688f7d9b
Bhupendra Malla <bhupendramalla321@gmail.com>
2025-02-05 08:12:53 +0545

    refactor: remove auth

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,17 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import { AuthProvider } from './contexts/AuthContext';
+// import { AuthProvider } from './contexts/AuthContext';
 import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthProvider>
+      {/* <AuthProvider> */}
         <App />
         <Toaster position="top-right" />
-      </AuthProvider>
+      {/* </AuthProvider> */}
     </BrowserRouter>
   </StrictMode>
 );
